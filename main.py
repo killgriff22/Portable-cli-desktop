8d18b7cd190fb3f2799bcb646fbfb7d0e53da31b
killgriff22 <kilgrif22033@gmail.com>
2024-04-14 19:05:47 -0400

    test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from functions import *
+last_message = ""
 while True:
-    screen.blit("Hello, World!", (0, 0))
+    screen.fill(" ")
+    screen.blit(last_message, (0, 0))
+    event = MultiTerm.window.get_event()
+    if event:
+        if event.type == MultiTerm.asciimaticsEvent.MouseEvent:
+            last_message = "Mouse event at: " + str(event)
     screen.draw()
