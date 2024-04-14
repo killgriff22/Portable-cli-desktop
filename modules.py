9b95033dc5710a3d7c3f9c88e786cfcc978aea2b
killgriff22 <kilgrif22033@gmail.com>
2024-04-14 19:07:27 -0400

    configure touch screen to register mouse events

--- modules.py
+++ modules.py
@@ -1,3 +1,8 @@
+import os
+os.environ['SDL_VIDEODRIVER'] = 'fbcon'
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+os.environ["SDL_MOUSEDRV"] = "TSLIB"
 import MultiTerm
 display_size = MultiTerm.os.get_terminal_size()
 screen = MultiTerm.Screen(display_size,(0,0))
